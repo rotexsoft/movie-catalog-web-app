80ceaa1eacdacf1e5875cd52a3c4d31fe85c3ff0
rotimi <rotimi@localhost>
2024-02-03 00:02:17 -0700

    Slim 4 Compatible

--- src/views/http-not-allowed-not-found-server-error-handler/index.php
+++
@@ -1,3 +0,0 @@
-You have successfully executed MovieCatalog\Controllers\HttpNotAllowedNotFoundServerErrorHandler::actionIndex()
-<br>
-This is the default view for MovieCatalog\Controllers\HttpNotAllowedNotFoundServerErrorHandler::actionIndex().
